ff6a4b6a48c5172597dbf02ae273963e20d49611
Yasha Gozwan Shuhada <yashagozwan13@gmail.com>
2024-03-27 12:22:31 +0700

    update

--- capp_case/lib/main.dart
+++ capp_case/lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:app_core/app_core.dart';
+import 'package:capp_case/firebase_options.dart';
 import 'package:capp_case/src/app.dart';
 import 'package:capp_case/src/core/di/di.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
+  final widgetBinding = WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await appCoreConfigureDependencies();
   await cappCaseConfigureDependencies();
   runApp(const App());
