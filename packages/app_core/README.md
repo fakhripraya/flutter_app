71cf98d8b22857f0acfb3503982e734014b9eb0f
Yasha Gozwan Shuhada <yashagozwan13@gmail.com>
2024-03-27 11:05:05 +0700

    update

--- packages/app_core/README.md
+++ packages/app_core/README.md
@@ -1,1 +1,8 @@
 # App Core
+
+## Dependencies
+- freezed_annotation
+- json_annotation
+- cloud_firestore
+- sign_in_google
+- firebase_auth
--- packages/app_core/lib/app_core.dart
+++ packages/app_core/lib/app_core.dart
@@ -1,7 +1,3 @@
 library app_core;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+
