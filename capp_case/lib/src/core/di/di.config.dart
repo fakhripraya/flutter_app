0b60e2d050c22bc0fab663c91f3d856b7921ff24
Fakhri Prayatna <50196922+fakhripraya@users.noreply.github.com>
2024-03-28 01:55:15 +0700

    di fix

--- capp_case/lib/src/core/di/di.config.dart
+++ capp_case/lib/src/core/di/di.config.dart
@@ -8,10 +8,12 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:app_core/app_core.dart' as _i6;
+import 'package:app_core/app_core.dart' as _i7;
 import 'package:capp_case/src/core/services/navigation_service.dart' as _i3;
 import 'package:capp_case/src/presentation/screens/home/bloc/home_bloc.dart'
     as _i5;
+import 'package:capp_case/src/presentation/screens/login/bloc/login_bloc.dart'
+    as _i6;
 import 'package:capp_case/src/presentation/screens/splash/bloc/splash_bloc.dart'
     as _i4;
 import 'package:get_it/get_it.dart' as _i1;
@@ -33,6 +35,10 @@ extension GetItInjectableX on _i1.GetIt {
         () => _i4.SplashBloc(gh<_i3.NavigationService>()));
     gh.lazySingleton<_i5.HomeBloc>(
         () => _i5.HomeBloc(gh<_i3.NavigationService>()));
+    gh.lazySingleton<_i6.LoginBloc>(() => _i6.LoginBloc(
+          gh<_i3.NavigationService>(),
+          gh<_i7.AuthUseCase>(),
+        ));
     return this;
   }
 }
