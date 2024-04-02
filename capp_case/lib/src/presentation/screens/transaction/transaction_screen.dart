3d501aeb23b4d3279a926efe53a2013110f4a1d0
Yasha Gozwan Shuhada <yashagozwan13@gmail.com>
2024-04-02 16:26:56 +0700

    update

--- capp_case/lib/src/presentation/screens/transaction/transaction_screen.dart
+++ capp_case/lib/src/presentation/screens/transaction/transaction_screen.dart
@@ -22,6 +22,36 @@ class _TransactionScreenState extends State<TransactionScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: Theme.of(context).iconTheme.copyWith(color: Colors.white),
+        titleTextStyle: const TextStyle(
+          fontSize: 16,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        title: const Text('Kebutuhan Primary'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(24),
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black,
+              ),
+            ),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text('12 April'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
