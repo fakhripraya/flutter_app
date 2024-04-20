b33e1473bc24fb3e89785fb427d7ba41eb9d1e3b
Yasha Gozwan Shuhada <yashagozwan13@gmail.com>
2024-04-20 23:44:31 +0700

    update

--- packages/app_core/lib/src/core/di/di.config.dart
+++ packages/app_core/lib/src/core/di/di.config.dart
@@ -8,13 +8,13 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:app_core/src/domain/auth/auth_interactor.dart' as _i8;
+import 'package:app_core/src/domain/auth/auth_interactor.dart' as _i12;
 import 'package:app_core/src/domain/auth/auth_repository.dart' as _i5;
-import 'package:app_core/src/domain/auth/auth_use_case.dart' as _i7;
+import 'package:app_core/src/domain/auth/auth_use_case.dart' as _i11;
 import 'package:app_core/src/domain/transaction/transaction_interactor.dart'
     as _i14;
 import 'package:app_core/src/domain/transaction/transaction_repository.dart'
-    as _i11;
+    as _i9;
 import 'package:app_core/src/domain/transaction/transaction_use_case.dart'
     as _i13;
 import 'package:app_core/src/infrastructure/auth/auth_remote_data_source.dart'
@@ -24,11 +24,11 @@ import 'package:app_core/src/infrastructure/auth/auth_remote_data_source_impl.da
 import 'package:app_core/src/infrastructure/auth/auth_repository_impl.dart'
     as _i6;
 import 'package:app_core/src/infrastructure/transaction/transaction_remote_data_source.dart'
-    as _i9;
+    as _i7;
 import 'package:app_core/src/infrastructure/transaction/transaction_remote_data_source_impl.dart'
-    as _i10;
+    as _i8;
 import 'package:app_core/src/infrastructure/transaction/transaction_repository_impl.dart'
-    as _i12;
+    as _i10;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
@@ -47,14 +47,14 @@ extension GetItInjectableX on _i1.GetIt {
         () => _i4.AuthRemoteDataSourceImpl());
     gh.lazySingleton<_i5.AuthRepository>(
         () => _i6.AuthRepositoryImpl(gh<_i3.AuthRemoteDataSource>()));
-    gh.lazySingleton<_i7.AuthUseCase>(
-        () => _i8.AuthInteractor(gh<_i5.AuthRepository>()));
-    gh.lazySingleton<_i9.TransactionRemoteDataSource>(
-        () => _i10.TransactionRemoteDataSourceImpl());
-    gh.lazySingleton<_i11.TransactionRepository>(() =>
-        _i12.TransactionRepositoryImpl(gh<_i9.TransactionRemoteDataSource>()));
+    gh.lazySingleton<_i7.TransactionRemoteDataSource>(
+        () => _i8.TransactionRemoteDataSourceImpl());
+    gh.lazySingleton<_i9.TransactionRepository>(() =>
+        _i10.TransactionRepositoryImpl(gh<_i7.TransactionRemoteDataSource>()));
+    gh.lazySingleton<_i11.AuthUseCase>(
+        () => _i12.AuthInteractor(gh<_i5.AuthRepository>()));
     gh.lazySingleton<_i13.TransactionUseCase>(
-        () => _i14.TransactionInteractor(gh<_i11.TransactionRepository>()));
+        () => _i14.TransactionInteractor(gh<_i9.TransactionRepository>()));
     return this;
   }
 }
