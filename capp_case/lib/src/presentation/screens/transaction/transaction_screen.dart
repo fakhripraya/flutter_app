b064118478d2daf3b8b7d22335421a1f37edc318
yashagozwan <yashagozwan13@gmail.com>
2024-04-22 15:37:21 +0700

    update

--- capp_case/lib/src/presentation/screens/transaction/transaction_screen.dart
+++ capp_case/lib/src/presentation/screens/transaction/transaction_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:ui';
+
 import 'package:capp_case/src/presentation/screens/transaction/bloc/transaction_bloc.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class TransactionScreen extends StatefulWidget {
@@ -37,21 +42,146 @@ class _TransactionScreenState extends State<TransactionScreen> {
       body: ListView(
         padding: const EdgeInsets.all(24),
         children: [
-          Container(
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.black,
+          _cardWidget(),
+          const SizedBox(height: 16),
+          _expanseAndIncomeWidget(),
+          const SizedBox(height: 24),
+          _expanseWidget(),
+        ],
+      ),
+    );
+  }
+
+  Widget _cardWidget() {
+    return Container(
+      padding: const EdgeInsets.all(24),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(
+          color: Colors.black,
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Text(
+            '12 APRIL 2024',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(height: 16),
+          Row(
+            children: [
+              Expanded(
+                child: _titleAndSubtitle(
+                  title: 'TOTAL EXPANSE',
+                  subtitle: 'Rp400.000.000',
+                ),
+              ),
+              Expanded(
+                child: _titleAndSubtitle(
+                  title: 'TOTAL INCOME',
+                  subtitle: 'Rp400.000.000',
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 16),
+          _titleAndSubtitle(
+            title: 'MY CURRENT AMOUNT',
+            subtitle: 'Rp400.000.000',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _titleAndSubtitle({
+    required String title,
+    required String subtitle,
+  }) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(subtitle),
+      ],
+    );
+  }
+
+  Widget _expanseAndIncomeWidget() {
+    return Container(
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(
+          color: Colors.black,
+        ),
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(24),
+              color: Colors.black,
+              child: const Text(
+                'Add Expense',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
               ),
             ),
-            child: const Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text('12 April'),
-              ],
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(24),
+              child: const Text(
+                'Add Income',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
             ),
           ),
         ],
       ),
+    );
+  }
+
+  Widget _expanseWidget() {
+    return const Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Expanse',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            Text(
+              'More',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
